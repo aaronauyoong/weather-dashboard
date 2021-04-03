f64bccfd84dfcdc1f8ca3991f3f3e28b20130257
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-04-03 19:13:36 +0800

    removed searchInput id in HTML, added pseudo code in JS, added 2 variables. JS still WIP

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Define main variables
+let citySearchInput = document.querySelector("#citySearchInput");
+
+// Empty array for search history
+let cityHistory = [];
+
+// Search button
+
+// Get weather API
+
+// Save search history to local storage
