1617468406c89491a5eea4028228a53f9fcd79de
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-04-07 00:02:56 +0800

    rearranged HTML, added css styling for UV Index risk, cleaned script file, all current weather data now being displayed. 5 Day forecast function, UV index colour coding, saving search to localStorage are WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,81 +7,116 @@ let citySearchHistory = [];
 // Define main variables
 let dailyWeatherResults = document.getElementById("searchResults");
 let forecastResults = document.getElementById("city5DayForecast");
-let citySearchInput = document.querySelector("#citySearchInput");
-let cityWeatherForm = document.querySelector("#cityWeatherSearchForm");
+let citySearch = document.getElementById("citySearchInput");
+let cityWeatherForm = document.getElementById("cityWeatherSearchForm");
+let currentWeatherIcon = document.getElementById("weatherIcon");
+let citySearchBtn = document.getElementById("searchBtn");
 
 const currentWeatherArticle = document.getElementById("todayWeatherContainer");
 const fiveDayArticle = document.getElementById("fiveDayForecastContainer");
 
-// let currentWeatherEl = document.querySelector("#todayWeatherContainer");
+const cityName = document.getElementById("thisCity");
+const currentTemp = document.getElementById("temperature");
+const currentHumidity = document.getElementById("humidity");
+const currentWindSpeed = document.getElementById("windSpeed");
+
+function dateTime() {
+    date = moment(new Date())
+    datetime.html(date.format("Do MMMM YYYY"));
+};
+
+$(document).ready(function () {
+    datetime = $('#dateNow')
+    dateTime();
+});
 
 // Retrieve today's weather from OpenWeather's API
-function getWeatherToday(cityName) {
+function getWeatherToday(citySearch) {
     
-    // URL
-    const weatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "Sydney" + "&appid=" + weatherAPIKey;
+    const weatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "Kuala Lumpur" + "&appid=" + weatherAPIKey;
 
-    // Fetching weather data from OpenWeather API
     fetch(weatherAPIUrl)
-        // method: 'GET',
-        // headers: {
-        //     'accept' : 'application/json',
-        // }
     .then((response) => response.json())
-    .then(function (data){
+    .then(function (data) {
+        
+        // Retrieving latitude and longitude for UV Index perusal
+        const latitude = data.coord.lat;
+        const longitude = data.coord.lon;
+        const weatherForecastURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly&appid=" + weatherAPIKey;
         
         displayCurrentWeather(data);
 
-        // displayFiveDayForecast(data);
+        fetch(weatherForecastURL)            
+        .then(function (response) {
+            if (response.error) {
+                throw new Error("Unable to obtain weather forecast data"); 
+            } 
+            
+            return response.json();
+            
+        }).then(function (data) {
+
+            console.log(data);
+            console.log(data.current.uvi);
+            console.log(data.current.temp);
+
+            displayUVI(data);
+            displayFiveDayForecast(data);
+        }).catch(error => {
+            console.log("error: ", error)
+        });
     });
 };
 
-// Function to display today's weather data
-function displayCurrentWeather(data){
+// Function to display UV Index and weather icon (linked to same onecall API)
+function displayUVI(data) {
 
-    // API Results as constants
-    const cityName = document.getElementById("thisCity");
-    const currentTemp = document.getElementById("temperature");
-    // converting temperature from Kelvin to °C, by deducting 273.15
-        // used JS toFixed(1) function to convert number to string with 1 decimal place
-    const celsiusTemp = (data.main.temp - 273.15).toFixed(1);
+    const currentUVIndex = document.getElementById("uvIndexRisk");
+    const apiWeatherIcon = data.current.weather[0].icon;
+    
+    currentWeatherIcon.src = "https://openweathermap.org/img/wn/" + apiWeatherIcon + ".png";
+    currentUVIndex.innerHTML = data.current.uvi;
 
-    const currentHumidity = document.getElementById("humidity");
+    if (currentUVIndex < 2) {
+        currentUVIndex.classList.add("uvLow");
+    } else if (currentUVIndex >= 2 && currentUVIndex <= 5) {
+        currentUVIndex.classList.add("uvModerate");
+    } else if (currentUVIndex >= 6 && currentUVIndex <= 7) {
+        currentUVIndex.classList.add("uvHigh");
+    } else if (currentUVIndex >= 8 && currentUVIndex <= 10) {
+        currentUVIndex.classList.add("uvVeryHigh");
+    } else {
+        currentUVIndex.classList.add("uvExtreme");
+    }
 
-    const currentWindSpeed = document.getElementById("windSpeed");
-    // converting wind speed from meters per second to km per hour, through multiplying value by 3.6
-        // used JS toFixed(1) function to convert number to string with 1 decimal place
-    const windSpeedKMH = (data.wind.speed * 3.6).toFixed(1);
-    console.log(windSpeedKMH);
+    // Insert five day forecast function here
+}; 
 
-    const currentUVIndex = document.getElementById("uvIndex");
-
-    // Populating results into respective html elements
-    cityName.innerHTML = data.name + ", " + data.sys.country;
-    currentTemp.innerHTML = celsiusTemp;
-    currentHumidity.innerHTML = data.main.humidity;
-    currentWindSpeed.innerHTML = windSpeedKMH;
+function displayFiveDayForecast(data) {
 
 };
 
-//         const data = await response.json();
+function displayCurrentWeather(data){
 
-//         // Display weather results in html divs todayWeatherContainer and city5DayForecast 
+    const kelvinTemp = data.main.temp;
+    const windSpeedMs = data.wind.speed;
 
-//         // const weatherImg = document.currentWeatherArticle.createElement('img');
+    // Populating results into respective html elements
+    cityName.innerHTML = data.name + ", " + data.sys.country;
+    currentTemp.innerHTML = CELSIUS_CONVERSION(kelvinTemp).toFixed(1);
+    currentHumidity.innerHTML = data.main.humidity;
+    currentWindSpeed.innerHTML = KmH_CONVERSION(windSpeedMs).toFixed(1);
+};
 
-//         // img.setAttribute('src', data.icon);
+// Converting from Kelvin to Degrees Celsius
+function CELSIUS_CONVERSION(kelvinTemp) {
+    return kelvinTemp - 273.15;
+};
 
-//         // imgArticle.appendChild(weatherImg);
-
-//         currentTemp.textContent = data.temp;
-//         currentHumidity.textContent = data.humidity;
-//         currentWindSpeed.textContent = data.speed + "km/h";
-
-//     })
-
-
-// };
+// Converting from Meters per Second to Kilometers per Hour
+function KmH_CONVERSION(windSpeedMs) {
+    return windSpeedMs * 3.6;
+};
 
 getWeatherToday();
 
@@ -96,65 +131,6 @@ function show5DayForecast() {
 show5DayForecast();
 showCurrentWeather();
 
-    // // Fetching today's weather from OpenWeather API
-    // fetch(weatherAPIUrl).then(function (response) {
-    //     if (response.ok) {
-    //         return response.json()
-    //     } else {
-    //         return Promise.reject("Failed to retrieve today's weather data. Please try again.");
-    //     }
-    // }).then (async (response) => {
-
-    //     // This section will filter out the received data to populate the html div cityForecastNow
-
-    //     let weatherIcon = data.weather[0].icon;
-    //     let iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
-
-    //     $("#weather-icon").attr("src", iconUrl);
-    // });
-
-// Get five day forecast from API
-// function getFiveDayForecast() {
-
-// };
-
-// Function to get today's date and time upon page load
-function dateTime() {
-    date = moment(new Date())
-    datetime.html(date.format("Do MMMM YYYY"));
-};
-
-$(document).ready(function () {
-    datetime = $('#dateNow')
-    dateTime();
-});
-
-// Display 5 day forecast
-
-// function displayFiveDayForecast() {
-
-// };
-
-// Search button
-// function cityWeatherSearch(event) {
-//     event.preventDefault();
-
-//     let city = citySearchInput.value.trim();
-
-//     if(city){
-//         getWeatherToday(city);
-//         getFiveDayForecast(city);
-//         citySearchHistory.unshift({city});
-//         citySearchInput.value = "";
-//     } else {
-//         window.alert("Please enter a city name in the search bar.");
-//     };
-
-//     // Functions for saving search and adding it to searchHistory
-//     // saveSearchHistory();
-//     displaySearchHistory(city);
-// };
-
 // Function to save search history to localStorage
 // function saveSearchHistory() {
 //     localStorage.setItem("cityHistory", JSON.stringify(citySearchHistory));
@@ -163,8 +139,26 @@ $(document).ready(function () {
 // Display search history as buttons on left side
 
 // function displaySearchHistory() {
-
 // };
 
+// Search Button
+
+// citySearchBtn.click(function () {
+
+//     if (cityName === "") {
+//         return;
+//     }
+//     getWeatherToday(cityName);
+//     // Add function for five day forecast
+//     // // 
+//     // Add function for storing to localStorage
+// });
+
+// cityWeatherForm.click(function (event) {
+//     event.preventDefault();
+//     citySearchBtn.click();
+// });
+
+
 // Event listeners
-// cityWeatherForm.addEventListener("submit", getWeatherToday);
+// $(cityWeatherSearchForm).click("submit", getWeatherToday);
